67a4ccebb6d023a5e7402a3b4566e0518cd9b1fd
Yasser Friha <friha.yasser@pm.me>
2020-02-26 23:15:20 +0100

    heroku 8

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', "young-hamlet-74827"]
+ALLOWED_HOSTS = ['127.0.0.1', "young-hamlet-74827.herokuapp.com"]
 
 
 # Application definition
