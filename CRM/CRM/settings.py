a3560fcddbecaeba85993b34f1424b7778608231
Yasser Friha <friha.yasser@pm.me>
2020-02-27 16:32:01 +0100

    heroku e

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -141,4 +141,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
