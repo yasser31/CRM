ad06a256274d21a702c3eb0d1214aaa064bd9484
Yasser Friha <friha.yasser@pm.me>
2020-03-17 18:53:03 +0100

    resetpsw1

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -148,7 +148,7 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
