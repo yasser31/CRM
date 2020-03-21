57800cdd5b559e057700316ad9b564f7e5d98ac6
Yasser Friha <friha.yasser@pm.me>
2020-03-21 14:59:47 +0100

    title not unique

--- CRM/meetings/models.py
+++ CRM/meetings/models.py
@@ -6,7 +6,7 @@ from contacts.models import Contact
 class Meeting(models.Model):
     ''' meeting report model '''
     date = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, unique=True, default='')
+    title = models.CharField(max_length=100, default='')
     summary = models.TextField(null=True)
     contact = models.ForeignKey(Contact, on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
