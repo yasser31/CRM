17a7e3c9cbc18514ac719da87ba2d329ea059f61
Yasser Friha <friha.yasser@pm.me>
2020-03-21 12:36:02 +0100

    correcting 500 error 6

--- CRM/contacts/views.py
+++ CRM/contacts/views.py
@@ -228,6 +228,6 @@ def p_404(request, exception):
     return render(request, "404.html", status=404)
 
 
-#def p_500(request):
-#''' 500 view '''
+# def p_500(request):
+# ''' 500 view '''
 # return render(request, "500.html", status=500)
