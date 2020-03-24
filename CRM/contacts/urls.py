86fce8aebf6f5b221e076573cb8e3c47637de85a
Yasser Friha <friha.yasser@pm.me>
2020-03-24 04:47:35 +0100

    third modification

--- CRM/contacts/urls.py
+++ CRM/contacts/urls.py
@@ -6,7 +6,8 @@ from . import views
 urlpatterns = [
     path('contacts/', views.contacts, name="contacts"),
     path('clients/', views.clients, name="clients"),
-    path('add_contact/<int:company_id>', views.add_contact, name="add_contact"),
+    path('add_contact/<int:company_id>', views.add_contact,
+         name="add_contact"),
     path('add_company/', views.add_company, name="add_company"),
     path('thanks/', views.thanks, name="thanks"),
     path('details/<int:company_id>', views.details, name="details"),
